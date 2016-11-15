8bffebb66b7b37ed2de650c015dbfa5e4420f54d
flo <flo.macicasan@gmail.com>
2016-11-15 21:01:19 +0200

    added username reading

---
+++ src/MyFirstJavaProgram.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * @author flo
+ * @since 15/11/2016.
+ */
+public class MyFirstJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Username: ");
+        String username = readValueFromConsole();
+        System.out.println("Welcome " + username + "!");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static String readValueFromConsole() {
+        return new Scanner(System.in).nextLine();
+    }
+}
